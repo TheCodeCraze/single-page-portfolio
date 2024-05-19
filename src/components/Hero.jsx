cf5b8c1a25ea6b8735781347a9a8fa805e787dbf
Tamil Iniyan S <tamiliniyans1999@gmail.com>
2024-05-19 12:15:31 +0530

    Completed tablet design for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,23 +4,25 @@ import Header from "./Header";
 const Hero = () => {
   return (
     <section className={styles.hero}>
-      <Header inHero={true} />
       <div className={styles.content}>
-        <div className={styles.text}>
-          <h2 className={styles.title}>
-            Nice to meet you! I&apos;m <span>Adam Keyes</span>.
-          </h2>
-          <p className={styles.description}>
-            Based in the UK, I&apos;m a front-end developer passionate about
-            building accessible web apps that users love.
-          </p>
-          <button className={styles.cta}>Contact me</button>
+        <Header inHero={true} />
+        <div className={styles.info}>
+          <div className={styles.text}>
+            <h2 className={styles.title}>
+              Nice to meet you! I&apos;m <span>Adam Keyes</span>.
+            </h2>
+            <p className={styles.description}>
+              Based in the UK, I&apos;m a front-end developer passionate about
+              building accessible web apps that users love.
+            </p>
+            <button className={styles.cta}>Contact me</button>
+          </div>
+          <img
+            src="/image-profile-desktop.webp"
+            alt="Adam Keyes's profile"
+            className={styles.image}
+          />
         </div>
-        <img
-          src="/image-profile-desktop.webp"
-          alt="Adam Keyes's profile"
-          className={styles.image}
-        />
       </div>
     </section>
   );
